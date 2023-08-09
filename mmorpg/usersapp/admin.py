560f9f96b44b92a85ece68ce69257fe5e70a2bd4
Elena-Pers <myworkmaill@mail.ru>
2023-08-09 17:41:45 +0700

    Initial commit

---
+++ mmorpg/usersapp/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import User, Post, Category, Comment, EmailVerification
+
+
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Comment)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'created')
+
+
+@admin.register(EmailVerification)
+class EmailVerificationAdmin(admin.ModelAdmin):
+    list_display = ('code', 'user', 'expiration')
+    fields = ('code', 'user', 'expiration', 'created')
+    readonly_fields = ('created',)
